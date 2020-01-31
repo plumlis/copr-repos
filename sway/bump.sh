85cc897f7640ff543943283ccf4b0adcd7312112
Jarkko Oranen <oranenj@iki.fi>
2020-01-31 21:39:59 +0200

    ???

--- sway/bump.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-REPO=swaywm/sway
-SPEC=sway.spec
-SHA=$(curl -s https://api.github.com/repos/$REPO/commits/master | jq -r .sha)
-echo $SHA
-sed -i "s/%global commit          .*/%global commit          $SHA/" $SPEC
